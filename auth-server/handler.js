3d4d81c449f7a83dcf362f45f1a3e8a6c21b3cd9
julianegoe <goersch.juliane@dpa.com>
2021-06-01 22:15:09 +0200

    it's futile

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,9 +1,8 @@
-const {google} = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
-/* const calendar = google.calendar("v3"); */
+const axios = require('axios');
+const querystring = require("querystring")
 
-/* const uri = "https://accounts.google.com/o/oauth2/auth?client_id=856026410494-63hkfdrkcoqmrqn79lg2b5e71rvui4q1.apps.googleusercontent.com&response_type=token&redirect_uri=https://julianegoe.github.io/CF_meet-app/&scope=https://www.googleapis.com/auth/calendar.readonly"
- */
+
+
 const credentials = {
   client_id : process.env.CLIENT_ID,
   project_id: process.env.PROJECT,
@@ -13,24 +12,18 @@ const credentials = {
   token_uri:"https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris:"https://julianegoe.github.io/CF_meet-app/",
-  javascript_origins:["https://julianegoe.github.io", "http://localhost:3000"]
+  javascript_origins:["https://julianegoe.github.io", "http://localhost:8080"]
 };
-
 const {client_id, client_secret, redirect_uris} = credentials;
 
 
-module.exports.getAuthUrl = async () => {
+module.exports.getAuthUrlNoLib = async () => {
 
-  const oauth2Client = new OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris
-  );
-
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/calendar.readonly']
-  });
+const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${client_id}&
+response_type=code&
+redirect_uri=${redirect_uris}&
+scope=https://www.googleapis.com/auth/calendar.readonly&
+access_type=offline`
 
   return {
     statusCode: 200,
@@ -39,44 +32,34 @@ module.exports.getAuthUrl = async () => {
   }
 };
 
-module.exports.getAccessToken = async (event) => {
-  console.log(event);
-  const oauth2Client = new OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris
-  );
-  const code = decodeURIComponent(`${event.pathParameters.code}`);
-  /* const code = "4%2F0AY0e-g44-jYS9SaUYvTMftoIn-Re-7svI9v9puu69-xNFuesBwib63k_PDtP4PCjaw-6PQ" */
-  return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-     */
+module.exports.getAccessTokenNoLib = async (event) => {
+  
+  /* const authCode = decodeURIComponent(`${event.pathParameters.code}`); */
+    
+  const authCode = decodeURIComponent('4%2F0AY0e-g78jor-F-NJYRC0txl5j2BIvRafAHEdjpPwj1IC6NNWVjsJTZwXfyHnRpGdSIVHmQ');
 
-     oauth2Client.getToken(code, (err, token) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(token);
-    });
-  })
-    .then((token) => {
-      // Respond with OAuth token
-      return {
-        statusCode: 200,
-        body: JSON.stringify(token),
-      };
-    })
-    .catch((err) => {
-      // Handle error
-      console.error(err);
-      return {
-        statusCode: 500,
-        body: JSON.stringify(err),
-      };
-    });
+  const data = querystring.stringify({
+    code: authCode,
+    client_id: client_id,
+    client_secret: client_secret,
+    redirect_uri: redirect_uris,
+    grant_type: 'authorization_code'
+  }); 
+
+  const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+  };
+
+  const endpoint = `https://oauth2.googleapis.com/token/`
+  
+  try {
+    const response = await axios.post(endpoint, data, headers)
+    return {
+      statusCode: 200,
+      body: response.data
+    }
+  } catch (err) {
+    return {body: "this is err: " + err}
+  }
+    
 }
-
-
-
